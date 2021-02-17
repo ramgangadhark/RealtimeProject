397cb4b422ebf486c3288fdcc7e9f2b33d0c312b
Ram Gangadhar <ramgangadhar_k@outlook.com>
2021-02-17 19:12:32 +0530

    Added SignOut

---
+++ Pods/Target Support Files/Pods-RealtimeProject-RealtimeProjectUITests/Pods-RealtimeProject-RealtimeProjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealtimeProject_RealtimeProjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RealtimeProject_RealtimeProjectUITests
+@end
---
+++ RealtimeProject/SignOutViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SignOutViewController.swift
+//  RealtimeProject
+//
+//  Created by Ram on 17/02/21.
+//
+
+import UIKit
+
+class SignOutViewController: UIViewController {
+
+    @IBOutlet weak var signOutBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        signOutBtn.layer.cornerRadius = 10
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
